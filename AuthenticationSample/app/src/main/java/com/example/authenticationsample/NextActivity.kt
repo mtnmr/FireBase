64c803b04ad992b83e5c8a8a79942d9be5fbf7c8
mtnmr <mei.rila1013@gmail.com>
2022-09-16 16:31:23 +0900

    email send

--- AuthenticationSample/app/src/main/java/com/example/authenticationsample/NextActivity.kt
+++ AuthenticationSample/app/src/main/java/com/example/authenticationsample/NextActivity.kt
@@ -2,6 +2,7 @@ package com.example.authenticationsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.authenticationsample.databinding.ActivityNextBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -15,6 +16,7 @@ class NextActivity : AppCompatActivity() {
         binding = ActivityNextBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //ユーザー情報の取得
         val user = Firebase.auth.currentUser
         val name = user?.displayName
         binding.username.text = getString(R.string.current_username, name)
@@ -22,5 +24,14 @@ class NextActivity : AppCompatActivity() {
         binding.email.text = getString(R.string.current_email, email)
         val uid = user?.uid
         binding.uid.text = getString(R.string.current_uid, uid)
+
+        //確認メールを送信する(コンソールでテンプレートを設定できる)
+        binding.sendEmailButton.setOnClickListener {
+            user?.sendEmailVerification()?.addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+                    Toast.makeText(this, "email send", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
